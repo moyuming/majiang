542f3ca5021a4b03b990b672e22a1e59c981f5b2
moyumin <347307741@qq.com>
2025-09-10 23:52:06 +0800

    更新代码

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,11 @@ import vue from '@vitejs/plugin-vue2'
 
 // https://vite.dev/config/
 export default defineConfig({
+  build: {
+    // outDir: 'dist', // 默认值，可以省略
+    // outDir: 'public', // 改为 public
+    outDir: 'docs',   // 改为 docs（适合GitHub Pages）
+    // outDir: 'output', // 改为 output
+  },
   plugins: [vue()],
 })
